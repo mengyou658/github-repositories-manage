d5a2b6496cdd2828b16c84602f6363d73420f014
yun <yun@admin.com>
2020-09-29 09:18:14 +0800

    * init

--- github-repositories-manage.js
+++ github-repositories-manage.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github项目批量删除
 // @namespace    https://github.com/mengyou658/github-repositories-manage
-// @version      1.1.1
+// @version      1.1.2
 // @description  Github项目批量删除，fork的项目太多了
 // @author       yunchaoq/mengyou658
 // @license      GPL License
@@ -13,7 +13,7 @@
 // @grant        GM_setClipboard
 // @grant        GM_getResourceText
 // ==/UserScript==
-(function() {
+(function () {
   'use strict';
   GM_addStyle(GM_getResourceText('layerCss'))
   // GM_addStyle(GM_getResourceText('layuiCss'))
@@ -23,13 +23,16 @@
   var checkList = [];
   var tmpCode = sessionStorage.getItem("gitto");
 
-  if (location.indexOf("?tab=repositories") > -1){
+  if (location.indexOf("?tab=repositories") > -1) {
     init();
   }
 
   function init() {
     if (!tmpCode) {
-      layer.prompt({title: '输入申请的token，默认一次使用，关闭浏览器后失效，并确认<a href="https://github.com/settings/tokens" target="_blank">点击这里是申请，请至少选择Delete Repos</a>', formType: 1}, function(token, index){
+      layer.prompt({
+        title: '输入申请的token，默认一次使用，关闭浏览器后失效，并确认<a href="https://github.com/settings/tokens" target="_blank">点击这里是申请，请至少选择Delete Repos</a>',
+        formType: 1
+      }, function (token, index) {
         if (token) {
           tmpCode = token;
           sessionStorage.setItem('gitto', tmpCode);
@@ -37,7 +40,7 @@
         }
       });
     }
-    $("#user-repositories-list li").each((id, it) =>{
+    $("#user-repositories-list li").each((id, it) => {
       $(it).append('<input type="checkbox" class="user-repositories-list-checkbox" name="repo-check" value="" >');
     });
 
@@ -48,7 +51,7 @@
       if (href.indexOf(pathname) > -1) {
         // var repoName = href.replace(pathname + "/", "");
         let index = checkList.indexOf(href);
-        if (index > -1 ){
+        if (index > -1) {
           find.prop('checked', false);
           checkList.splice(index, 1);
         } else {
@@ -59,37 +62,49 @@
           $(".UnderlineNav-body:first").append('<a href="javascript:void(0);" id="user-repositories-delete" class="UnderlineNav-item btn btn-primary ">批量删除</a>');
           $("#user-repositories-delete").on('click', function () {
             var resList = {};
-            checkList.forEach((it)=> {
-              var url = 'https://api.github.com/repos' + it
-              $.ajax({
-                url: url,
-                method: 'DELETE',
-                async: false,
-                "headers": {
-                  "Accept": "application/vnd.github.v3+json",
-                  "Authorization": "token " + tmpCode
-                },
-                success: function (data, textStatus, jqXHR) {
-                  resList[it] = true;
-                }, error: function (XMLHttpRequest, textStatus, errorThrown) {
-                  resList[it] = false;
-                }
-              })
+            var count = 0;
+            var promiseList = [];
+            checkList.forEach((it) => {
+              var tmpPromise = new Promise((resolve, reject) => {
+                var url = 'https://api.github.com/repos' + it
+                $.ajax({
+                  url: url,
+                  method: 'DELETE',
+                  "headers": {
+                    "Accept": "application/vnd.github.v3+json",
+                    "Authorization": "token " + tmpCode
+                  },
+                  success: function (data, textStatus, jqXHR) {
+                    resList[it] = true;
+                    count++;
+                    resolve(count);
+                  }, error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    resList[it] = false;
+                    count++;
+                    resolve(count);
+                  }
+                })
+              });
+              promiseList.push(tmpPromise);
             })
-            var resTxt = "";
-            for (var i in resList) {
-              if (resList[i] == true) {
-                resTxt += " 删除【" + i + "】成功<br />";
+
+            Promise.all(promiseList).then((count) => {
+              var resTxt = "";
+              for (var i in resList) {
+                if (resList[i] == true) {
+                  resTxt += " 删除【" + i + "】成功<br />";
+                }
               }
-            }
-            for (var i in resList) {
-              if (resList[i] == false) {
-                resTxt += " 删除【" + i + "】失败<br />";
+              for (var ii in resList) {
+                if (resList[ii] == false) {
+                  resTxt += " 删除【" + ii + "】失败<br />";
+                }
               }
-            }
-            layer.msg('删除结果：<br />' + resTxt, function () {
-              window.location.reload();
+              layer.msg('删除结果：<br />' + resTxt, function () {
+                window.location.reload();
+              });
             });
+
           });
         }
       }
